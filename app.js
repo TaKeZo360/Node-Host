f3b70fca8c0dfb2dc73529976dc9bd3c6c1f739b
TaKeZo360 <mdreja003@gmail.com>
2025-11-30 21:09:38 +0530

    Prepare app for Render deployment

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const data = require("./data/data.json");
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 
 app.get('/data', (req,res)=> {
   res.status(200).send(data.name);
